43a5a3bf1a865d44a5d903fd3da9d494b13bbca1
yejian_mac <yejian@jovision.com>
2019-07-07 00:09:47 +0900

    init

---
+++ ios_example/ios_example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_example
+//
+//  Created by prudence on 2019/07/07.
+//  Copyright © 2019 prudence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
